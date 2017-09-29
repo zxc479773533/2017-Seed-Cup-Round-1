1e3bf6242bf144bd13976d9a0bbfb102119dfa66
qjy981010 <1114943038@qq.com>
2017-09-29 19:35:20 +0800

    with auc

--- DNN/tf_test2.py
+++ DNN/tf_test2.py
@@ -159,7 +159,7 @@ def train(training_set, label_set):
 
 def predict(classifier, team_data, win_rate, pca):
     # Classify two new flower samples.
-    new_samples, labels, history = loadData('../seedcupTask/matchDataTest.csv', team_data, win_rate)
+    new_samples, labels, history = loadData('../matchDataTest.csv', team_data, win_rate)
     new_samples = pca.transform(new_samples)
     predict_input_fn = tf.estimator.inputs.numpy_input_fn(
         x={"x": np.array(new_samples)},
@@ -178,7 +178,7 @@ if __name__ == '__main__':
     team_data = np.load('team_data.npy')
     win_rate = np.load('team_win_rate.npy')
     training_set, label_set, history = loadData(
-        '../2017-Seed-Cup-Round-1/matchDataTrain.csv', team_data, win_rate)
+        '../matchDataTrain.csv', team_data, win_rate)
     classifier, pca = train(training_set, label_set)
     predict(classifier, team_data, win_rate, pca)
 
